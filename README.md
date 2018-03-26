c5c81a1535b639ae8028e8032f0780db8ccd9c67
Stanislav Tkachenko <wmts@arkadium.com>
2018-03-26 11:25:33 -0400

    demo mode

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # Arenax SDK
 
+- [Arenax SDK](#arenax-sdk)
+  - [Intro](#intro)
+  - [Project structure](#project-structure)
+
+## Intro
+
 Arena SDK helps you integrate games into game Arena. You can use it as starting template for you project but this is not necessary. You can just copy necessary parts to your application, this will work as well.
 
 Arena project based on redux, you can use [Redux devtools](http://extension.remotedev.io/) to track communications between Arena and Game
+
+## Project structure
+
+Project assembly based on webpack, but it is not necessary, you can use any web packing technology as soon as it does the same. Main requirements for resulting package described here [webpack.config.js](webpack.config.js)
+
 
 Entry point for the arena game defined here:
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-
+const gamesJson = require('./previews/arenax-preview/games.json')
 module.exports = {
   devtool: 'inline-source-map',
   devServer: {
-    inline: true
+    inline: true,
+    before(app) {
+      app.get('/games/', function (req, res) {
+        if(req.query.locale)
+        {
+          res.json(gamesJson);
+        }
+      });
+      app.get('/games/demo', function (req, res) {
+          res.sendFile(__dirname + '/previews/arenax-preview/index.html')
+      });
+    }
   },
   entry: './entry/arenax/main.js',
   output: {
     publicPath: '/',
     filename: '[name].min.js',
     libraryTarget: 'var',
-    library: ['__arenax_v1__', 'BridgeGame']
+    library: ['__arenax_v1__', 'DemoGame']
   },
   plugins: [
     new CopyWebpackPlugin(
